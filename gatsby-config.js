c7fcc6615e6ae97100b9d82c6b5331dd5300d27a
Joel <zimmi.94@live.de>
2023-04-08 18:18:45 +0200

    ~feat(netlify) adds netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-postcss',
     `gatsby-plugin-image`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
