72900ed777a940dca8d4f1c49fdcfeb6b78a1b78
Joel <zimmi.94@live.de>
2023-04-12 20:46:45 +0200

    ~feat(picture-gallery) adds hover effect on picture

--- src/components/Gallery/gallery-picture.tsx
+++ src/components/Gallery/gallery-picture.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+
 import { Cloudinary } from "@cloudinary/url-gen";
 import { fill, scale, thumbnail } from "@cloudinary/url-gen/actions/resize";
 import { coloumnWidthFunction } from "./gallery-grid";
@@ -17,7 +18,7 @@ const GalleryPicture: React.FunctionComponent<GalleryPictureProps> = ({ picture,
     const columnWidth = coloumnWidthFunction(windowWidth);
     const maxSpan = Math.floor(windowWidth / columnWidth);
     const pictureColumnWidth = picture.width / maxSpan;
-    
+
     const span = Math.min(Math.floor(picture.width / picture.height), maxSpan);
     const amountOfGap = 5 * (span - 1);
 
@@ -25,16 +26,22 @@ const GalleryPicture: React.FunctionComponent<GalleryPictureProps> = ({ picture,
     var imageTage;
     if (maxSpan == span) {
         imageTage = cloudinaryImage
-        .resize(crop().width((pictureColumnWidth * span) + amountOfGap).height(pictureColumnWidth))
-        .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
+            .resize(crop().width((pictureColumnWidth * span) + amountOfGap).height(pictureColumnWidth))
+            .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
     } else {
         imageTage = cloudinaryImage
-        .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
+            .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
     }
 
     //tailwind uses col-span-1 col-span-2 col-span-3 col-span-4 col-span -5
-    return (<div key={index} className={`not-prose col-span-${span}`} >
+    return (<div key={index} className={`group not-prose col-span-${span} relative`} >
         <AdvancedImage cldImg={imageTage} alt={picture.alt} />
+        <div className="hidden group-hover:block absolute top-0 bg-base-100/60 px-2">
+            {picture.caption}
+        </div>
+        <div className="hidden group-hover:block absolute bottom-0 bg-base-100/60 w-full px-2">
+            {picture.alt}
+        </div>
     </div>)
 
 }
