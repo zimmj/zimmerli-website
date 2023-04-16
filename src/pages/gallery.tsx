789d775d1a586b230f6c73e3036abd3677d39d2f
Joel <zimmi.94@live.de>
2023-04-16 22:24:44 +0200

    ~refactor(gallery) move calculation of picture size to parent, acoid reload on resize

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import GalleryGrid, { coloumnWidthFunction } from "../components/Gallery/gallery-grid";
@@ -7,6 +7,15 @@ import GalleryPicture, { PictureFormat } from "../components/Gallery/gallery-pic
 import { useWindowWidth } from "@react-hook/window-size";
 import SlideInModal from "../components/Slide-In-Modal/slide-in-modal";
 import DetailPicture from "../components/Gallery/detail-picture";
+import { Cloudinary, CloudinaryImage } from "@cloudinary/url-gen";
+import { crop, thumbnail } from "@cloudinary/url-gen/actions/resize";
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'jzimmj'
+  }
+});
+
 
 const LandScapeGellery: React.FunctionComponent = ({ data }) => {
   const windowWidth = useWindowWidth();
@@ -23,27 +32,54 @@ const LandScapeGellery: React.FunctionComponent = ({ data }) => {
     setIsOpen(false);
   }
 
-  const pictures = data.allCloudinaryMedia.edges.map((node: any, index: number) => {
-    const media: ICloudinaryMedia = node.node;
-    const picture: PictureFormat = {
-      id: media.publicId,
-      url: media.secure_url,
-      width: media.originalWidth,
-      height: media.originalHeight,
-      alt: media.cloudinaryData.context.custom.alt,
-      caption: media.cloudinaryData.context.custom.caption
-    };
-    return (
-      <GalleryPicture
-        key={index}
-        index={index}
-        picture={picture}
-        windowWidth={windowWidth}
-        openModal={onOpenModal}
-      />
-    )
-  });
+  const columnWidth = coloumnWidthFunction(windowWidth);
+  const maxSpan = Math.floor(windowWidth / columnWidth);
 
+
+  function calculatePicture(maxSpan: number, width: number, span: number, id: string): CloudinaryImage {
+    const pictureColumnWidth = width / maxSpan;
+    const amountOfGap = 5 * (span - 1);
+    const cloudinaryImage = cld.image(id);
+    if (maxSpan == span) {
+      return cloudinaryImage
+        .resize(crop().width((pictureColumnWidth * span) + amountOfGap).height(pictureColumnWidth))
+        .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
+    } else {
+      return cloudinaryImage
+        .resize(thumbnail().width((columnWidth * span) + amountOfGap).height(columnWidth));
+    }
+  }
+
+  function calculatePictures(): React.ReactNode {
+    const pictures: React.ReactNode = data.allCloudinaryMedia.edges.map((node: any, index: number) => {
+      const media: ICloudinaryMedia = node.node;
+      const span = Math.min(Math.floor(media.originalWidth/ media.originalHeight), maxSpan);
+      const pictureTag = calculatePicture(
+        maxSpan, media.originalWidth, span , media.publicId);
+      const picture: PictureFormat = {
+        id: media.publicId,
+        pictureTag: pictureTag,
+        span: span,
+        width: media.originalWidth,
+        height: media.originalHeight,
+        alt: media.cloudinaryData.context.custom.alt,
+        caption: media.cloudinaryData.context.custom.caption
+      };
+      return (
+        <GalleryPicture
+          key={index}
+          index={index}
+          picture={picture}
+          columnWidth={columnWidth}
+          openModal={onOpenModal}
+        />
+      )
+    });
+    return pictures;
+  }
+
+  const pictures: React.ReactNode = useMemo(() => calculatePictures(), [columnWidth, maxSpan]);
+  
   return (
     <Layout>
       <Seo title="Picture Collection of Lanscapes from different travels" />
