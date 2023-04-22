23c236008ef23368dcddaa6168309cb9ac362a4b
Joel <zimmi.94@live.de>
2023-04-22 12:40:31 +0200

    ~fix(slidein-state): Removes wrong width class

--- src/components/Slide-In-Modal/slide-in-modal.tsx
+++ src/components/Slide-In-Modal/slide-in-modal.tsx
@@ -14,7 +14,7 @@ const SlideInModal: React.FunctionComponent<SlideInModalProps> = ({ children, is
 
 
     return (
-        <div className={`slide-in transition-all ${isOpen ? 'duration-0' : 'duration-300'} ${isOpen ? 'w-full' : 'w-0'} fixed h-screen w-screen bg-slate-400/60 top-0 right-0 z-30`} onClick={onClose}>
+        <div className={`slide-in transition-all ${isOpen ? 'duration-0' : 'duration-300'} ${isOpen ? 'w-full' : 'w-0'} fixed h-screen bg-slate-400/60 top-0 right-0 z-30`} onClick={onClose}>
             <section onClick={(event) => event.stopPropagation()} className={`transition-all duration-300 ${isOpen ? 'w-full right-0 lg:w-5/6 xl:w-4/6 2xl:w-3/6' : 'w-0'}  bg-primary fixed h-full `} >
                 <div className={`${isOpen ? 'absolute' : ' hidden'} btn btn-sm btn-circle right-5 top-5`} onClick={onClose}>✕</div>
                 {children}
